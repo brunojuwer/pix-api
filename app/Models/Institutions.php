fb4fd985c35439e64af7921a76d056dad389f7a6
Bruno Juwer <bruno.juwer1997@gmail.com>
2023-11-08 09:03:45 -0300

    feat(models): add models relations

--- app/Models/Institutions.php
+++ app/Models/Institutions.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Institutions extends Model
 {
     use HasFactory;
+
+    public function api_keys(): HasOne
+    {
+        return $this->hasOne(ApiKeys::class);
+    }
 }
--- app/Models/Keys.php
+++ app/Models/Keys.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Keys extends Model
 {
     use HasFactory;
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Clients::class);
+    }
+
+    public function institution(): BelongsTo
+    {
+        return $this->belongsTo(Institutions::class);
+    }
 }
